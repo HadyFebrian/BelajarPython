fedb3241b8737e94fd416486dff55d81d75a6451
HadyFebrian <hadyhfrian@gmail.com>
2024-03-11 10:24:23 +0700

    Initial Commit

---
+++ Tipe Data Primitif.py
@@ -0,0 +1,31 @@
+#Interger
+x = 10
+print(type(x))
+#Float
+x = 1.0
+print(type(x))
+#Complex
+x = 1+1J
+print(type(x))
+#Boolean
+x = True
+print(type(x))
+x = False
+print(type(x))
+#String
+x = 'Belajar'
+print(type(x))
+x = "Python"
+print(type(x))
+#Formatted String
+nama = "Hady Febrian"
+Umur = 25
+print(f"Hello, nama saya {nama}, Umur saya {Umur}")
+#%-Formatting
+nama = ("Hady Febrian")
+Umur = 25
+print("Nama saya %s," % (nama),"Umur Saya %s" % (Umur))
+#String Formatting
+Nama = "Hady Febrian"
+Umur = 25
+print("Nama Saya {},".format(Nama),"Umur Saya {}.".format(Umur))
