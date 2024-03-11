a7b640b4653a148b75b8d898c4c0043544fae91a
HadyFebrian <hadyhfrian@gmail.com>
2024-03-11 10:09:43 +0700

    Initial Commit

---
+++ Input, Output, &Comment.py
@@ -0,0 +1,13 @@
+# Proses Input
+Nama = input("Masukkan Nama Anda :")
+"""
+Proses Output
+"""
+print(Nama)
+'''
+Contoh Lain
+'''
+Usia =input('Masukkan Usia Anda :')
+print(Usia)
+#(Tanda #, """kata""",'''kata''', adalah Comment)
+
