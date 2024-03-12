39bbc708e710c62c9dad0325395be2c12799f0ef
HadyFebrian <hadyhfrian@gmail.com>
2024-03-12 20:59:27 +0700

    Initial Commit

---
+++ Perulangan.py
@@ -0,0 +1,17 @@
+#Menampilkan Angka 1-10
+#For
+for i in range(1,11):
+    print(i)
+#While
+i=0
+while i<=10:
+    print(i)
+    i+=1
+#Nested Loop
+for i in range(1,11):
+    for j in range(1,11):
+        print(i,j)
+
+evenNumber = []
+evenNumber = [i + 2 for i in range(-2, 500,2)]
+print(evenNumber)
