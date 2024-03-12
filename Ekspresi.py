905432076eb9f79981e9c9eaeefdbeb5fa13578d
HadyFebrian <hadyhfrian@gmail.com>
2024-03-12 11:01:28 +0700

    Initial Commit

---
+++ Ekspresi.py
@@ -0,0 +1,28 @@
+#Contoh Penerapan Ekspresi
+x = 99
+y = 20
+z = 82
+hasil = x-z+y
+print(hasil)
+
+#Contoh Penggabungan Angka dan Huruf +
+angka = (1,2,3,4,5)
+huruf = ("HADY","FEBRIAN")
+gabung = angka + huruf
+print(gabung)
+#Contoh Penggabungan Angka dan Huruf *
+kali = huruf * 2
+print(kali)
+#Uner
+a = True
+a = not a
+print(a)
+b = 10
+b -= 5
+print(b)
+c = 10
+c += 5
+print(c)
+d = 10
+print(-d)
+
