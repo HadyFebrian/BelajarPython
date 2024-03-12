0c9e555e1f0ba8802a19ed21d36025a1ce1b9e3a
HadyFebrian <hadyhfrian@gmail.com>
2024-03-12 11:21:53 +0700

    Initial Commit

---
+++ Jenis - Jenis Operator.py
@@ -0,0 +1,61 @@
+#Operator Aritmatika
+x = 99
+y = 10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+#Operator Relasional
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+#Penerapan Operator Relasional Pada String
+x = "HADY"
+y = "FEBRIAN"
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+#Operator Logika
+#AND
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+#OR
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+#NOT
+print(not True)
+print(not False)
+#Operator Assigment
+# +=
+x = 99
+x += 10
+print(x)
+# -=
+x = 99
+x -= 10
+print(x)
+# *=
+x = 99
+x *= 10
+print(x)
+# /=
+x = 99
+x /= 10
+print(x)
+#%=
+x = 99
+x%= 10
+print(x)
